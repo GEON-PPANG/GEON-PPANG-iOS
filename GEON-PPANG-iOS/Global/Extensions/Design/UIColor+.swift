7be77309e8c8ded2228dfc9fa5b3b021da2863d7
jeongdung-eo <jeuniis2@gmail.com>
2023-07-03 02:17:11 +0900

    [Add] #6 - Design Extension 추가

---
+++ GEON-PPANG-iOS/Global/Extensions/Design/UIColor+.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/07/03.
+//
+
+import Foundation
---
+++ GEON-PPANG-iOS/Global/Extensions/Design/UIImage+.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage+.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/07/03.
+//
+
+import Foundation
