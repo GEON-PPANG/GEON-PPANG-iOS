ef6f3132c4dc43655f7c559b1dec299140e1a3a9
seongmin221 <seongmin221@naver.com>
2023-07-05 16:19:20 +0900

    [Feat] #19 - DescriptionCollectionViewCell added

---
+++ GEON-PPANG-iOS/Presentation/Common/Cell/DescriptionCollectionViewCell.swift
@@ -0,0 +1,70 @@
+//
+//  DescriptionCollectionViewCell.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 2023/07/05.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class DescriptionCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Property
+    
+    var tagText: String
+    
+    // MARK: - UI Property
+    
+    private let descriptionLabel = UILabel()
+    
+    // MARK: - Life Cycle
+    
+    init(text: String) {
+        self.tagText = text
+        
+        super.init(frame: .zero)
+        
+        setLayout()
+        setStyle()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setting
+    
+    private func setLayout() {
+        addSubview(descriptionLabel)
+        descriptionLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(5)
+            $0.verticalEdges.equalToSuperview().inset(1)
+        }
+    }
+    
+    private func setStyle() {
+        self.do {
+            // TODO: asset 추가 시 변경
+            $0.makeBorder(width: 0.5, color: .systemGray)
+            $0.makeCornerRound(radius: 3)
+        }
+        
+        descriptionLabel.do {
+            $0.text = tagText
+            $0.font = .pretendardMedium(13)
+            // TODO: asset 추가 시 변경
+            $0.textColor = .systemGray
+        }
+    }
+    
+    // MARK: - Custom Method
+    
+    func configureTag(_ text: String) {
+        descriptionLabel.text = text
+    }
+    
+}
