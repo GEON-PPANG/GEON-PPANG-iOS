0f8c44a832f4c5f3b1483ea6fa274c48a0950fb1
ckkim817 <ckkim817@konkuk.ac.kr>
2023-07-06 14:49:37 +0900

    [Setting] #17 - UIColor extension 추가

--- GEON-PPANG-iOS/Global/Extensions/Design/UIColor+.swift
+++ GEON-PPANG-iOS/Global/Extensions/Design/UIColor+.swift
@@ -2,7 +2,32 @@
 //  UIColor+.swift
 //  GEON-PPANG-iOS
 //
-//  Created by JEONGEUN KIM on 2023/07/03.
+//  Created by kyun on 2023/07/06.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    
+    // 사용 예시)
+    // $0.textColor = Color.main1
+    // $0.backgroundColor = Color.background1
+    
+    static let main1 = UIColor(named: "Main_1")
+    static let main2 = UIColor(named: "Main_2")
+    static let main3 = UIColor(named: "Main_3")
+    static let point1 = UIColor(named: "Point_1")
+    static let point2 = UIColor(named: "Point_2")
+    static let error = UIColor(named: "Error")
+    static let background1 = UIColor(named: "Background_1")
+    static let background2 = UIColor(named: "Background_2")
+    static let gray1 = UIColor(named: "gray-50")
+    static let gray2 = UIColor(named: "gray-100")
+    static let gray3 = UIColor(named: "gray-200")
+    static let gray4 = UIColor(named: "gray-300")
+    static let gray5 = UIColor(named: "gray-400")
+    static let gray6 = UIColor(named: "gray-500")
+    static let gray7 = UIColor(named: "gray-600")
+    static let gray8 = UIColor(named: "gray-700")
+    static let gray9 = UIColor(named: "gray-800")
+}
