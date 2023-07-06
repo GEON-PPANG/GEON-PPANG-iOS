1dc8c12b763da662424aaa0e9aedcaac3551ead9
ckkim817 <ckkim817@konkuk.ac.kr>
2023-07-06 15:30:48 +0900

    [Fix] #17 - 가독성을 위한 UIColor+ 상수명 수정
    예시) .main1 -> .gbbMain1

--- GEON-PPANG-iOS/Global/Extensions/Design/UIColor+.swift
+++ GEON-PPANG-iOS/Global/Extensions/Design/UIColor+.swift
@@ -10,24 +10,28 @@ import UIKit
 extension UIColor {
     
     // 사용 예시)
-    // $0.textColor = Color.main1
-    // $0.backgroundColor = Color.background1
+    // $0.textColor = .gbbMain1
+    // $0.backgroundColor = .gbbBackground1
     
-    static let main1 = UIColor(named: "Main_1")
-    static let main2 = UIColor(named: "Main_2")
-    static let main3 = UIColor(named: "Main_3")
-    static let point1 = UIColor(named: "Point_1")
-    static let point2 = UIColor(named: "Point_2")
-    static let error = UIColor(named: "Error")
-    static let background1 = UIColor(named: "Background_1")
-    static let background2 = UIColor(named: "Background_2")
-    static let gray1 = UIColor(named: "gray-50")
-    static let gray2 = UIColor(named: "gray-100")
-    static let gray3 = UIColor(named: "gray-200")
-    static let gray4 = UIColor(named: "gray-300")
-    static let gray5 = UIColor(named: "gray-400")
-    static let gray6 = UIColor(named: "gray-500")
-    static let gray7 = UIColor(named: "gray-600")
-    static let gray8 = UIColor(named: "gray-700")
-    static let gray9 = UIColor(named: "gray-800")
+    static let gbbMain1 = UIColor(named: "Main_1")
+    static let gbbMain2 = UIColor(named: "Main_2")
+    static let gbbMain3 = UIColor(named: "Main_3")
+    
+    static let gbbPoint1 = UIColor(named: "Point_1")
+    static let gbbPoint2 = UIColor(named: "Point_2")
+    
+    static let gbbError = UIColor(named: "Error")
+    
+    static let gbbBackground1 = UIColor(named: "Background_1")
+    static let gbbBackground2 = UIColor(named: "Background_2")
+    
+    static let gbbGray1 = UIColor(named: "gray-50")
+    static let gbbGray2 = UIColor(named: "gray-100")
+    static let gbbGray3 = UIColor(named: "gray-200")
+    static let gbbGray4 = UIColor(named: "gray-300")
+    static let gbbGray5 = UIColor(named: "gray-400")
+    static let gbbGray6 = UIColor(named: "gray-500")
+    static let gbbGray7 = UIColor(named: "gray-600")
+    static let gbbGray8 = UIColor(named: "gray-700")
+    static let gbbGray9 = UIColor(named: "gray-800")
 }
