13e9bf3e1acb59250771c95bb508d68490699953
seongmin221 <seongmin221@naver.com>
2023-07-07 02:46:18 +0900

    [Feat] #16 - optionsCollectionView in progress

---
+++ GEON-PPANG-iOS/Presentation/WriteReview/OptionsCollection/OptionsCollectionViewCell.swift
@@ -0,0 +1,96 @@
+//
+//  OptionsCollectionViewCell.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 2023/07/07.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class OptionsCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Property
+    
+    enum CellStatus {
+        case deselected
+        case selected
+        case disabled
+    }
+    
+    var status: CellStatus {
+        didSet {
+            configureCell()
+        }
+    }
+    
+    var cellText: String
+    
+    private var cellBorderColor: UIColor {
+        switch status {
+        case .deselected: return .gbbGray400!
+        case .selected: return .gbbMain3!
+        case .disabled: return .gbbGray300!
+        }
+    }
+    
+    private var cellTextColor: UIColor {
+        switch status {
+        case .deselected: return .gbbGray400!
+        case .selected: return .gbbMain1!
+        case .disabled: return .gbbGray300!
+        }
+    }
+    
+    // MARK: - UI Property
+    
+    private let cellLabel = UILabel()
+    
+    // MARK: - Life Cycle
+    
+    init(status: CellStatus, text: String) {
+        self.status = status
+        self.cellText = text
+        
+        super.init(frame: .zero)
+        
+        setLayout()
+        setUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setting
+    
+    private func setLayout() {
+        addSubview(cellLabel)
+        cellLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(12)
+            $0.verticalEdges.equalToSuperview().inset(8)
+        }
+    }
+    
+    private func setUI() {
+        self.do { $0.makeCornerRound(radius: 12.5) }
+        cellLabel.do { $0.text = cellText }
+        
+        configureCell()
+    }
+    
+    // MARK: - Custom Method
+    
+    private func configureCell() {
+        self.do { $0.makeBorder(width: 1.5, color: cellBorderColor) }
+        cellLabel.do { $0.textColor = cellTextColor }
+    }
+    
+    func toggleCellInteraction() {
+        self.isUserInteractionEnabled.toggle()
+    }
+    
+}
--- GEON-PPANG-iOS/Presentation/WriteReview/WriteReviewViewController.swift
+++ GEON-PPANG-iOS/Presentation/WriteReview/WriteReviewViewController.swift
@@ -20,7 +20,7 @@ final class WriteReviewViewController: BaseViewController {
     
     // TODO: navigationBar 추가
     private let bakeryOverviewView = BakeryOverviewView(bakeryImage: .actions, regions: ["tset", "efqerqf"])
-    
+    private let seperatorView = LineView()
     
     // MARK: - Setting
     
@@ -31,6 +31,21 @@ final class WriteReviewViewController: BaseViewController {
             $0.top.equalToSuperview()
             $0.height.equalTo(125)
         }
+        
+        view.addSubview(seperatorView)
+        seperatorView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.top.equalTo(bakeryOverviewView.snp.bottom)
+            $0.height.equalTo(1)
+        }
+    }
+    
+    override func setUI() {
+        
+    }
+    
+    override func setDelegate() {
+        
     }
     
 }
