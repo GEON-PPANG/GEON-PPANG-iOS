bb9458da8c8ed7e6ce20e7cf1f284a2362d72877
ckkim817 <ckkim817@konkuk.ac.kr>
2023-07-07 08:37:08 +0900

    [Fix] #10 - UIFont+에서 변수 -> 상수로 수정

--- GEON-PPANG-iOS/Global/Extensions/Design/UIFont+.swift
+++ GEON-PPANG-iOS/Global/Extensions/Design/UIFont+.swift
@@ -21,51 +21,27 @@ extension UIFont {
         return UIFont(name: "Pretendard-Medium", size: size)!
     }
     
-    class var largeTitle: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 32.0)!
-    }
+    static let largeTitle = UIFont(name: "Pretendard-Bold", size: 32.0)
+
+    static let title1 = UIFont(name: "Pretendard-Bold", size: 26.0)
     
-    class var title1: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 26.0)!
-    }
+    static let title2 = UIFont(name: "Pretendard-Bold", size: 20.0)
     
-    class var title2: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 20.0)!
-    }
+    static let headLine = UIFont(name: "Pretendard-Bold", size: 18.0)
     
-    class var headLine: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 18.0)!
-    }
+    static let subHead = UIFont(name: "Pretendard-Medium", size: 15.0)
     
-    class var subHead: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 15.0)!
-    }
+    static let bodyB1 = UIFont(name: "Pretendard-Bold", size: 17.0)
     
-    class var bodyB1: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 17.0)!
-    }
+    static let bodyM1 = UIFont(name: "Pretendard-Medium", size: 17.0)
     
-    class var bodyM1: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 17.0)!
-    }
+    static let bodyB2 = UIFont(name: "Pretendard-Bold", size: 14.0)
     
-    class var bodyB2: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 14.0)!
-    }
+    static let bodyM2 = UIFont(name: "Pretendard-Medium", size: 14.0)
     
-    class var bodyM2: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 14.0)!
-    }
+    static let captionB1 = UIFont(name: "Pretendard-Bold", size: 13.0)
     
-    class var captionB1: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 13.0)!
-    }
+    static let captionM1 = UIFont(name: "Pretendard-Medium", size: 13.0)
     
-    class var captionM1: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 13.0)!
-    }
-    
-    class var captionM2: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 11.0)!
-    }
+    static let captionM2 = UIFont(name: "Pretendard-Medium", size: 11.0)
 }
