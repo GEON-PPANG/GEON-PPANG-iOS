eaccd29cbc5ce22491190f5b798ff584d75e90a3
jeongdung-eo <jeuniis2@gmail.com>
2023-07-08 15:58:22 +0900

    [Add] #24 - 탭바에 포함되는 뷰컨트롤러 추가

---
+++ GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/BakeryListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BakeryListViewController.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/07/08.
+//
+
+import UIKit
+
+final class BakeryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
