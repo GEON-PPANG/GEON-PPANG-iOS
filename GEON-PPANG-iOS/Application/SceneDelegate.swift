17d75084635df89145df2146582c22209665e8de
seongmin221 <seongmin221@naver.com>
2023-07-08 14:09:24 +0900

    [Feat] #16 - reviewTextView added

--- GEON-PPANG-iOS/Application/SceneDelegate.swift
+++ GEON-PPANG-iOS/Application/SceneDelegate.swift
@@ -14,20 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
-                guard let _ = (scene as? UIWindowScene) else { return }
-                
-                if let windowScene = scene as? UIWindowScene {
-                    
-                    let window = UIWindow(windowScene: windowScene)
-                    window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
-                    
-                    let rootVC = ViewController()
-                    let navigationController = UINavigationController(rootViewController: rootVC)
-                    
-                    window.rootViewController = navigationController
-                    window.makeKeyAndVisible()
-                    self.window = window
-                }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController(rootViewController: WriteReviewViewController())
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
             }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- GEON-PPANG-iOS/Presentation/WriteReview/View/ReviewDetailTextView.swift
+++ GEON-PPANG-iOS/Presentation/WriteReview/View/ReviewDetailTextView.swift
@@ -5,4 +5,80 @@
 //  Created by 이성민 on 2023/07/07.
 //
 
-import Foundation
+import UIKit
+
+final class ReviewDetailTextView: UIView {
+    
+    // MARK: - Property
+    
+    var numOfWords: Int = 0
+    
+    // MARK: - UI Property
+    
+    let detailTextView = UITextView()
+    private let textLimitLabel = UILabel()
+    private let textMinimumLabel = UILabel()
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+        setUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setting
+    
+    private func setLayout() {
+        addSubview(detailTextView)
+        detailTextView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(196)
+        }
+        
+        addSubview(textMinimumLabel)
+        textMinimumLabel.snp.makeConstraints {
+            $0.top.equalTo(detailTextView.snp.bottom).offset(7)
+            $0.trailing.equalTo(detailTextView)
+        }
+        
+        addSubview(textLimitLabel)
+        textLimitLabel.snp.makeConstraints {
+            $0.top.equalTo(detailTextView.snp.bottom).offset(7)
+            $0.trailing.equalTo(textMinimumLabel.snp.leading).offset(4)
+        }
+    }
+    
+    private func setUI() {
+        detailTextView.do {
+            $0.contentInset = .init(top: 20, left: 28, bottom: 16, right: 28)
+            $0.font = .subHead
+            $0.textColor = .gbbGray600
+            $0.makeCornerRound(radius: 12)
+            $0.makeBorder(width: 1, color: .gbbGray400!)
+        }
+        
+        textLimitLabel.do {
+            $0.text = "\(numOfWords)/500"
+            $0.font = .captionM1
+            $0.textColor = .gbbGray400
+        }
+        
+        textMinimumLabel.do {
+            $0.text = "(최소 10자)"
+            $0.font = .captionM1
+            $0.textColor = .gbbGray400
+        }
+    }
+    
+    // MARK: - Custom Method
+    
+    
+    
+}
