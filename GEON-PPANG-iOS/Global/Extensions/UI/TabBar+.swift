0b852f22c30c6229f9e4378fa5ea19bf23f4ccf2
jeongdung-eo <jeuniis2@gmail.com>
2023-07-08 17:29:54 +0900

    [Add] #24 - 탭바 초기화 함수 extension 추가

---
+++ GEON-PPANG-iOS/Global/Extensions/UI/TabBar+.swift
@@ -0,0 +1,19 @@
+//
+//  TabBar+.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/07/08.
+//
+
+import UIKit
+
+extension UITabBar {
+    func setUpUITabBar() {
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        UITabBar.appearance().backgroundColor = UIColor.white
+        UITabBarItem.appearance().titlePositionAdjustment.vertical = -5
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont.pretendardBold(13), NSAttributedString.Key.foregroundColor: UIColor.gbbGray400!], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont.pretendardBold(13), NSAttributedString.Key.foregroundColor: UIColor.gbbMain3!], for: .normal)
+    }
+}
