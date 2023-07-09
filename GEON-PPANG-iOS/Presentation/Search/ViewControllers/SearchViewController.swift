5e3ee8676873302fc15ba2cd2a34355ba3dba243
jeongdung-eo <jeuniis2@gmail.com>
2023-07-09 21:54:58 +0900

    [Feat] #26 - search뷰 구현

---
+++ GEON-PPANG-iOS/Presentation/Search/ViewControllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/07/09.
+//
+
+import UIKit
+
+final class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+}
