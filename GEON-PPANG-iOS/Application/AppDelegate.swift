945b151fe6819bf5843f4c0922f6f8c6c4b270b5
ckkim817 <ckkim817@konkuk.ac.kr>
2023-07-11 01:06:36 +0900

    [Fix] #32 - Launch Screen sleep 시간 변경
    1 -> 1.5

--- GEON-PPANG-iOS/Application/AppDelegate.swift
+++ GEON-PPANG-iOS/Application/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        sleep(1)
+        sleep(UInt32(1.5))
         
         return true
     }
