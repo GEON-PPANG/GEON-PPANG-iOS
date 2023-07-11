873d8f57026d395346786d25dca0c30ab61eedb7
jeongdung-eo <jeuniis2@gmail.com>
2023-07-11 13:26:32 +0900

    [Feat] #36 - 빵집 리스트 필터뷰 구현

---
+++ GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/BakeryFilterView.swift
@@ -0,0 +1,160 @@
+//
+//  BakeryFilterView.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/07/11.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class BakeryFilterView: UIView {
+    
+    // MARK: - Property
+    
+    enum Section: Hashable {
+        case main
+    }
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, BakeryFilterItems>
+    private var dataSource: DataSource?
+    private var filterlist: [BakeryFilterItems] = BakeryFilterItems.item
+    
+    // MARK: - UI Property
+    
+    private lazy var filterCollectionview = UICollectionView(frame: .zero, collectionViewLayout: layout())
+    private lazy var filterButton = UIButton(configuration: .plain())
+    
+    private let topView = UIView()
+    private let lineView = UIView()
+    private let bottomView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setLayout()
+        setUI()
+        
+        setRegister()
+        setupDataSource()
+        setReloadData()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        
+        filterCollectionview.do {
+            $0.delegate = self
+            $0.showsHorizontalScrollIndicator = false
+        }
+        
+        filterButton.do {
+            $0.configuration?.background.strokeWidth = 1
+            $0.configuration?.background.strokeColor = .gbbGray200
+            $0.configuration?.baseForegroundColor = .black
+            $0.configuration?.image = .swapIcon
+            $0.configuration?.attributedTitle = AttributedString("기본순", attributes: AttributeContainer([.font: UIFont.pretendardBold(13)]))
+            $0.configuration?.cornerStyle = .capsule
+            $0.configuration?.imagePadding = 5
+            $0.configuration?.contentInsets = .zero
+            $0.addAction(UIAction { _ in
+                print("tapped")
+            }, for: .touchUpInside)
+        }
+        
+        lineView.do {
+            $0.backgroundColor = .gbbGray200
+        }
+        [topView, bottomView].forEach {
+            $0.backgroundColor = .gbbGray200
+        }
+    }
+    
+    private func setLayout() {
+        addSubviews(topView, filterButton, lineView, filterCollectionview, bottomView)
+        
+        topView.snp.makeConstraints {
+            $0.top.directionalHorizontalEdges.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+        
+        filterButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(24)
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(CGSize(width: 87, height: 36))
+        }
+        
+        lineView.snp.makeConstraints {
+            $0.width.equalTo(1)
+            $0.leading.equalTo(filterButton.snp.trailing).offset(12)
+            $0.directionalVerticalEdges.equalToSuperview().inset(15)
+        }
+        
+        filterCollectionview.snp.makeConstraints {
+            $0.leading.equalTo(lineView.snp.trailing)
+            $0.centerY.trailing.equalToSuperview()
+            $0.height.equalTo(42)
+        }
+        
+        bottomView.snp.makeConstraints {
+            $0.directionalHorizontalEdges.bottom.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+    }
+    
+    func layout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 10
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = .init(top: 0, left: 13, bottom: 0, right: 13)
+        return layout
+    }
+    
+    private func setRegister() {
+        filterCollectionview.register(cell: BakeryFilterCollectionViewCell.self)
+    }
+    
+    private func setupDataSource() {
+        dataSource = DataSource(collectionView: filterCollectionview, cellProvider: { collectionView, indexPath, item in
+            let cell: BakeryFilterCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+            cell.bind(item: item, index: indexPath.item)
+            return cell
+        })
+    }
+    
+    private func setReloadData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, BakeryFilterItems>()
+        defer { dataSource?.apply(snapshot, animatingDifferences: false)}
+        snapshot.appendSections([.main])
+        snapshot.appendItems(filterlist)
+    }
+}
+
+extension BakeryFilterView: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if self.filterlist[indexPath.item].status == .off {
+            self.filterlist[indexPath.item].status = .on
+        } else {
+            self.filterlist[indexPath.item].status = .off
+        }
+        self.filterlist[indexPath.item] = self.filterlist[indexPath.item].isSelected()
+    }
+}
+
+extension BakeryFilterView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth: CGFloat
+        let cell: BakeryFilterCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+        cell.filterTitle.text = filterlist[indexPath.row].filter.title
+        cell.getSize()
+        cellWidth = cell.filterTitle.frame.width + 48
+        
+        return CGSize(width: cellWidth, height: 36)
+    }
+}
