ea17411e2c3724db560b3b2e90cc44f5f2ed48d8
seongmin221 <seongmin221@naver.com>
2023-07-12 05:08:06 +0900

    [Feat] #39 - FilterViewControllers added

---
+++ GEON-PPANG-iOS/Presentation/Filter/ViewControllers/FilterIngredientViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FilterIngredientViewController.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 2023/07/12.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class FilterIngredientViewController: BaseViewController {
+    
+    // MARK: - Property
+    
+    
+    
+    // MARK: - UI Property
+    
+    
+    
+    // MARK: - Life Cycle
+    
+    
+    
+    // MARK: - Setting
+    
+    
+    
+    // MARK: - Action Helper
+    
+    
+    
+    // MARK: - Custom Method
+    
+    
+    
+}
