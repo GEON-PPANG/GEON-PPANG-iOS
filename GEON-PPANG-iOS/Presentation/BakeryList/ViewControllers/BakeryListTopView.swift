75a3651d07646f5bf6bd1a0423eb920728acfdfc
jeongdung-eo <jeuniis2@gmail.com>
2023-07-11 13:25:01 +0900

    [Feat] #36 - 빵집 리스트 상단뷰 구현

---
+++ GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/BakeryListTopView.swift
@@ -0,0 +1,77 @@
+//
+//  BakeryListTopView.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/07/11.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class BakeryListTopView: UIView {
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    private let hStackView = UIStackView()
+    private let bakeryTitle = UILabel()
+    private let bakeryIcon = UIImageView()
+    private let searchButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setLayout()
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        hStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 8
+        }
+        
+        bakeryTitle.do {
+            $0.basic(text: "건빵집 리스트",
+                     font: .pretendardBold(26),
+                     color: .gbbGray700!)
+        }
+        
+        bakeryIcon.do {
+            $0.image = .listIcon
+            $0.contentMode = .scaleAspectFit
+        }
+        
+        searchButton.do {
+            $0.setImage(.searchIcon600, for: .normal)
+        }
+    }
+    
+    private func setLayout() {
+        addSubviews(hStackView, searchButton)
+        hStackView.addArrangedSubviews(bakeryTitle, bakeryIcon)
+        
+        hStackView.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(24)
+            $0.bottom.equalToSuperview().offset(-15)
+            $0.top.equalToSuperview().offset(44)
+        }
+        
+        bakeryIcon.snp.makeConstraints {
+            $0.size.equalTo(24)
+        }
+        
+        searchButton.snp.makeConstraints {
+            $0.top.equalTo(hStackView.snp.top)
+            $0.trailing.equalToSuperview().offset(-23)
+            $0.bottom.equalToSuperview().offset(-15)
+        }
+    }
+}
