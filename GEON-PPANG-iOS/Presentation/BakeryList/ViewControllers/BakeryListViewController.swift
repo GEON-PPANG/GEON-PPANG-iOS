8b374398a24c5dca01973777ef0d4f4007d16168
jeongdung-eo <jeuniis2@gmail.com>
2023-07-11 14:05:27 +0900

    [Add] #36 - 빵집리스트 컬렉션뷰 뷰컨에 구현

--- GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/BakeryListViewController.swift
+++ GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/BakeryListViewController.swift
@@ -14,15 +14,30 @@ final class BakeryListViewController: BaseViewController {
         
     // MARK: - Property
     
+    enum Section {
+        case main
+    }
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, BakeryListResponseDTO>
+    private var dataSource: DataSource?
+    private var filterlist: [BakeryListResponseDTO] = BakeryListResponseDTO.item
     private lazy var safeArea = self.view.safeAreaLayoutGuide
     
     // MARK: - UI Property
     
     private let bakeryTopView = BakeryListTopView()
     private let bakeryFilterView = BakeryFilterView()
+    private lazy var bakeryListCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
 
     // MARK: - Life Cycle
     
+    override func viewDidLoad() {
+         super.viewDidLoad()
+        
+        setRegister()
+        setupDataSource()
+        setReloadData()
+        
+    }
     override func setUI() {
         bakeryFilterView.do {
             $0.backgroundColor = .clear
@@ -30,17 +45,51 @@ final class BakeryListViewController: BaseViewController {
     }
     
     override func setLayout() {
-        view.addSubviews(bakeryTopView, bakeryFilterView)
+        view.addSubviews(bakeryTopView, bakeryFilterView, bakeryListCollectionView)
         
         bakeryTopView.snp.makeConstraints {
             $0.top.equalTo(safeArea)
             $0.directionalHorizontalEdges.equalTo(safeArea)
             $0.height.equalTo(convertByHeightRatio(91))
         }
+        
         bakeryFilterView.snp.makeConstraints {
             $0.top.equalTo(bakeryTopView.snp.bottom)
             $0.directionalHorizontalEdges.equalTo(safeArea)
             $0.height.equalTo(convertByHeightRatio(72))
         }
+        
+        bakeryListCollectionView.snp.makeConstraints {
+            $0.top.equalTo(bakeryFilterView.snp.bottom)
+            $0.directionalHorizontalEdges.equalTo(safeArea)
+            $0.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setRegister() {
+        bakeryListCollectionView.register(cell: BakeryListCollectionViewCell.self)
+    }
+    
+    private func layout() -> UICollectionViewLayout {
+        var config = UICollectionLayoutListConfiguration(appearance: .plain)
+        config.backgroundColor = .clear
+        config.showsSeparators = true
+        
+        let layout = UICollectionViewCompositionalLayout.list(using: config)
+        return layout
+    }
+    
+    private func setupDataSource() {
+        dataSource = DataSource(collectionView: bakeryListCollectionView, cellProvider: { collectionView, indexPath, item in
+            let cell: BakeryListCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+            return cell
+        })
+    }
+    
+    private func setReloadData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, BakeryListResponseDTO>()
+        defer { dataSource?.apply(snapshot, animatingDifferences: false)}
+        snapshot.appendSections([.main])
+        snapshot.appendItems(filterlist)
     }
 }
