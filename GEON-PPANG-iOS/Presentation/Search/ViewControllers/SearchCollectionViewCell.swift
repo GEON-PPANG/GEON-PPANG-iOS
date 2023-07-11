22e602fe87f05cb8456aa5f4f860481d7645c612
jeongdung-eo <jeuniis2@gmail.com>
2023-07-12 06:36:20 +0900

    [Add] #40 - search 컬렉션뷰 셀 추가

---
+++ GEON-PPANG-iOS/Presentation/Search/ViewControllers/SearchCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SearchCollectionViewCell.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/07/12.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SearchCollectionViewCell: UICollectionViewCell {
+    
+}
