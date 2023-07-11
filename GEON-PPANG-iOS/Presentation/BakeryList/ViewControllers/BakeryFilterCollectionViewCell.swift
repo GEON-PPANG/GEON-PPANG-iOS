361831caf0cbc01498caf7ce99de072c3ac954db
jeongdung-eo <jeuniis2@gmail.com>
2023-07-11 13:25:43 +0900

    [Feat] #36 - 빵집 리스트 필터셀 구현

---
+++ GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/BakeryFilterCollectionViewCell.swift
@@ -0,0 +1,78 @@
+//
+//  BakeryFilterCollectionViewCell.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/07/11.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class BakeryFilterCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Property
+    
+    var isTapped: Bool = true
+    
+    // MARK: - UI Property
+    
+    private let hStackView = UIStackView()
+    private let iconView = UIImageView()
+     var filterTitle = UILabel()
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setLayout()
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setting
+    
+    private func setUI() {
+        contentView.do {
+            $0.makeCornerRound(radius: 18)
+            $0.makeBorder(width: 1, color: .gbbGray200!)
+            $0.backgroundColor = .gbbGray100
+        }
+        hStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 4
+            $0.distribution = .fillProportionally
+        }
+        filterTitle.do {
+            $0.font = .pretendardMedium(13)
+            $0.sizeToFit()
+        }
+    }
+    
+    private func setLayout() {
+        contentView.addSubview(hStackView)
+        hStackView.addArrangedSubviews(iconView, filterTitle)
+        
+        hStackView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(UIEdgeInsets(top: 8, left: 12, bottom: 8, right: 12))
+        }
+        
+        iconView.snp.makeConstraints {
+            $0.size.equalTo(20)
+        }
+    }
+    
+    func bind(item: BakeryFilterItems, index: Int) {
+        iconView.image = item.leftIcon
+        filterTitle.text = item.filter.title
+        filterTitle.textColor = (item.status == .off) ? .black : .gbbBackground2
+    }
+    func getSize() {
+        return filterTitle.sizeToFit()
+    }
+}
