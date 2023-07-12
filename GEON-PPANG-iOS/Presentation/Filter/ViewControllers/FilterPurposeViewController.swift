acf45f1a162d22b9e49ea1a975dbc464e9581018
seongmin221 <seongmin221@naver.com>
2023-07-12 14:26:31 +0900

    [Feat] #39 - FilterPurposeViewController edited

--- GEON-PPANG-iOS/Presentation/Filter/ViewControllers/FilterPurposeViewController.swift
+++ GEON-PPANG-iOS/Presentation/Filter/ViewControllers/FilterPurposeViewController.swift
@@ -15,12 +15,17 @@ final class FilterPurposeViewController: BaseViewController {
     // MARK: - Property
     
     private var maxSteps: Int = 0
+    private var userName: String = "Id"
+    private var filterTitleText: String = I18N.Filter.Purpose.title
+    private let filterCellTexts: [String] = FilterPurposeType.allCases.map { $0.description }
     
     // MARK: - UI Property
     
+    private let navigationBar = CustomNavigationBar()
     private let filterTitleLabel = UILabel()
-    private let filterSubtitleLabel = UILabel()
-    private let filterCollectionView = UICollectionView()
+    private let flowLayout = UICollectionViewFlowLayout()
+    private lazy var filterCollectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+    private let nextButton = CommonButton()
     
     // MARK: - Life Cycle
     
@@ -41,6 +46,56 @@ final class FilterPurposeViewController: BaseViewController {
         self.maxSteps = steps
     }
     
+    override func setLayout() {
+        view.addSubview(navigationBar)
+        navigationBar.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
+        }
+        
+        view.addSubview(filterTitleLabel)
+        filterTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        
+        view.addSubview(filterCollectionView)
+        filterCollectionView.snp.makeConstraints {
+            $0.top.equalTo(filterTitleLabel.snp.bottom).offset(40)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        view.addSubview(nextButton)
+        nextButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(55)
+        }
+    }
+    
+    override func setUI() {
+        navigationBar.do {
+            $0.addBackButtonAction(popViewControllerAction())
+            $0.configureRightCount(maxSteps - 2, by: maxSteps)
+        }
+        
+        filterTitleLabel.do {
+            $0.text = filterTitleText.insertString(userName, at: 5)
+            $0.font = .title1
+            $0.textColor = .gbbGray700
+        }
+        
+//        flowLayout.do {
+//            $0.
+//        }
+        
+        filterCollectionView.do {
+            $0.register(cell: FilterCollectionViewCell.self)
+        }
+    }
+    
+    override func setDelegate() {
+        filterCollectionView.delegate = self
+        filterCollectionView.dataSource = self
+    }
+    
     // MARK: - Action Helper
     
     
@@ -48,5 +103,25 @@ final class FilterPurposeViewController: BaseViewController {
     // MARK: - Custom Method
     
     
+    
+}
+
+// MARK: - extension
+
+extension FilterPurposeViewController: UICollectionViewDelegate {}
+
+// MARK: - extension
+
+extension FilterPurposeViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filterCellTexts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: FilterCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+        cell.filterType = .purpose
+        return cell
+    }
     
 }
