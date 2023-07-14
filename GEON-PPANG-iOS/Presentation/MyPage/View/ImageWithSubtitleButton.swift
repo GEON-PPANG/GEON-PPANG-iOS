ceee6377d8abe5a470b79c44bb773dd4d8270304
seongmin221 <seongmin221@naver.com>
2023-07-14 19:08:04 +0900

    [Feat] #51 - mypageDTO added

---
+++ GEON-PPANG-iOS/Presentation/MyPage/View/ImageWithSubtitleButton.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonWithSubtitleView.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 2023/07/14.
+//
+
+import Foundation
---
+++ GEON-PPANG-iOS/Presentation/MyPage/View/MyPageCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageCollectionViewCell.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 2023/07/14.
+//
+
+import Foundation
