e70f4014b0939101d4885032838ad45392efeaaa
jeongdung-eo <jeuniis2@gmail.com>
2023-07-15 21:59:30 +0900

    [Add] #42 - empty section 추가

--- GEON-PPANG-iOS/Presentation/MyReviews/ViewControllers/MyReviewsViewController.swift
+++ GEON-PPANG-iOS/Presentation/MyReviews/ViewControllers/MyReviewsViewController.swift
@@ -62,19 +62,44 @@ final class MyReviewsViewController: BaseViewController {
     
     private func setRegistration() {
         collectionView.register(cell: BakeryListCollectionViewCell.self)
+        collectionView.register(cell: EmptyCollectionViewCell.self)
         collectionView.register(header: MyReviewsHeaderView.self)
     }
-
+    
     private func layout() -> UICollectionViewLayout {
-        var config = UICollectionLayoutListConfiguration(appearance: .grouped)
-        config.backgroundColor = .clear
-        config.showsSeparators = true
-        config.headerMode = .supplementary
-        config.separatorConfiguration.topSeparatorVisibility = .hidden
-        config.separatorConfiguration.bottomSeparatorVisibility = .visible
-
-        let layout = UICollectionViewCompositionalLayout.list(using: config)
+        let layout = UICollectionViewCompositionalLayout {_, layoutEnvirnment  in
+            if self.myReviewslist.isEmpty {
+                return self.normalSection()
+            } else {
+                var config = UICollectionLayoutListConfiguration(appearance: .grouped)
+                config.backgroundColor = .clear
+                config.showsSeparators = true
+                config.headerMode = .supplementary
+                config.separatorConfiguration.topSeparatorVisibility = .hidden
+                config.separatorConfiguration.bottomSeparatorVisibility = .visible
+                
+                let layoutSection = NSCollectionLayoutSection.list(using: config, layoutEnvironment: layoutEnvirnment)
+                return layoutSection
+            }
+        }
         return layout
+    }
+    
+    private func normalSection() -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(layoutSize: .init(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1))
+        )
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: .init(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            ),
+            subitem: item,
+            count: 1
+        )
+        let section = NSCollectionLayoutSection(group: group)
+        return section
     }
 }
 
@@ -83,17 +108,28 @@ final class MyReviewsViewController: BaseViewController {
 extension MyReviewsViewController: UICollectionViewDataSource {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return myReviewslist.count
+        if myReviewslist.isEmpty {
+            return 1
+        } else {
+            return myReviewslist.count
+        }
     }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: BakeryListCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
-        cell.getViewType(.reviewType)
-        cell.updateUI(data: myReviewslist[indexPath.section], index: indexPath.item)
-        return cell
+        if myReviewslist.isEmpty {
+            let cell: EmptyCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+            cell.getViewType(.noReview)
+            return cell
+        } else {
+            let cell: BakeryListCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+            cell.getViewType(.reviewType)
+            cell.updateUI(data: myReviewslist[indexPath.section], index: indexPath.item)
+            return cell
+        }
     }
 }
 
@@ -101,7 +137,11 @@ extension MyReviewsViewController: UICollectionViewDataSource {
 
 extension MyReviewsViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let header: MyReviewsHeaderView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, indexPath: indexPath)
+        if myReviewslist.isEmpty {
+            return UICollectionReusableView()
+        } else {
+            let header: MyReviewsHeaderView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, indexPath: indexPath)
             return header
+        }
     }
 }
