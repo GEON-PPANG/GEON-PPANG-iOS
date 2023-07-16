2b7b281e835fe39bebfdd6dcc0209383d3b1e536
seongmin221 <seongmin221@naver.com>
2023-07-16 15:28:39 +0900

    [Feat] #58 - added stringliteral

--- GEON-PPANG-iOS/Global/Literals/Strings.swift
+++ GEON-PPANG-iOS/Global/Literals/Strings.swift
@@ -45,6 +45,10 @@ struct I18N {
             명예훼손, 욕설, 타인 비방글 등)은 안내 없이 삭제되며, 관련법률에 따라
             제재를 받을 수 있습니다. 또한, 건빵은 이에 대한 책임을 지지 않습니다.
         """
+        static let sheetTitle = "정말 리뷰 작성을 그만하시겠어요?"
+        static let sheetDescription = "고객님의 리뷰가 다른 고객님들에게\n도움이 될 수 있어요"
+        static let sheetQuit = "그만하기"
+        static let sheetContinue = "계속하기"
     }
     
     /// BakeryList
