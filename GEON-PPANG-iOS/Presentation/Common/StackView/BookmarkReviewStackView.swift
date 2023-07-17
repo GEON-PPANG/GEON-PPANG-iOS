679cb2037ac65e64b12498b14d3f06507ef5fcc6
ckkim817 <ckkim817@konkuk.ac.kr>
2023-07-18 05:45:04 +0900

    [Feat] #45 - CountStackView, BookmarkReviewStackView 구현

---
+++ GEON-PPANG-iOS/Presentation/Common/StackView/BookmarkReviewStackView.swift
@@ -0,0 +1,48 @@
+//
+//  BookmarkReviewStackView.swift
+//  GEON-PPANG-iOS
+//
+//  Created by kyun on 2023/07/17.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class BookmarkReviewStackView: UIStackView {
+
+    // MARK: - UIProperty
+    
+    private lazy var bookmarkCountStackView = CountStackView(type: .bookmark)
+    private lazy var reviewCountStackView = CountStackView(type: .review)
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setUI()
+        setLayout()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setting
+    
+    private func setUI() {
+        
+        self.do {
+            $0.axis = .horizontal
+            $0.spacing = 6
+            $0.distribution = .equalSpacing
+        }
+    }
+    
+    private func setLayout() {
+        
+        self.addArrangedSubviews(bookmarkCountStackView, reviewCountStackView)
+    }
+}
