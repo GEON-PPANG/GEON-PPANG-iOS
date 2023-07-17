0db1fe331d75715ca15a906a80a7019375816207
jeongdung-eo <jeuniis2@gmail.com>
2023-07-18 02:28:12 +0900

    [Add] #62 - gitignore에 configuration 추가

--- GEON-PPANG-iOS/Global/Network/Base/Config.swift
+++ GEON-PPANG-iOS/Global/Network/Base/Config.swift
@@ -12,6 +12,7 @@ enum Config {
     enum Keys {
         enum Plist {
             static let baseURL = "BASE_URL"
+            static let accessToken = "ACCESS_TOKEN"
         }
     }
     
@@ -28,6 +29,13 @@ extension Config {
     static let baseURL: String = {
         guard let key = Config.infoDictionary[Keys.Plist.baseURL] as? String else {
             fatalError("✅Base URL is not set in plist for this configuration.✅")
+        }
+        return key
+    }()
+    
+    static let accessToken: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.accessToken] as? String else {
+            fatalError("✅accessToken is not set in plist for this configuration.✅")
         }
         return key
     }()
