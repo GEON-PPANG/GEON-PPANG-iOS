27c44fb863c46151759d1032942d85243bfc2f84
seongmin221 <seongmin221@naver.com>
2023-07-17 18:52:35 +0900

    [Feat] #61 - filter completion view added

---
+++ GEON-PPANG-iOS/Presentation/Filter/ViewControllers/FilterCompleteViewController.swift
@@ -0,0 +1,79 @@
+//
+//  FilterCompleteViewController.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 2023/07/17.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class FilterCompleteViewController: BaseViewController {
+    
+    // MARK: - UI Property
+    
+    private let navigationBar = CustomNavigationBar()
+    private let welcomeLabel = UILabel()
+    private let welcomeImageView = UIImageView(image: .welcomeImage)
+    private lazy var startButton = CommonButton()
+    
+    // MARK: - Setting
+    
+    override func setLayout() {
+        view.addSubview(navigationBar)
+        navigationBar.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
+        }
+        
+        view.addSubview(welcomeLabel)
+        welcomeLabel.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        view.addSubview(welcomeImageView)
+        welcomeImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        view.addSubview(startButton)
+        startButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(CGFloat().heightConsideringBottomSafeArea(55))
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.height.equalTo(56)
+        }
+    }
+    
+    override func setUI() {
+        navigationBar.do {
+            $0.addBackButtonAction(UIAction { _ in
+                self.navigationController?.popViewController(animated: true)
+            })
+        }
+        
+        welcomeLabel.do {
+            $0.font = .title1
+            $0.textColor = .gbbGray700
+            $0.numberOfLines = 2
+            $0.setLineHeight(by: 1.03, with: I18N.Filter.welcomeTitle)
+        }
+        
+        startButton.do {
+            $0.getButtonTitle(.start)
+            $0.getButtonUI(.gbbMain2!)
+            $0.addAction {
+                self.startButtonTapped()
+            }
+        }
+    }
+    
+    // MARK: - Action Helper
+    
+    private func startButtonTapped() {
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
+        sceneDelegate?.changeRootViewControllerToTabBarController()
+    }
+    
+}
