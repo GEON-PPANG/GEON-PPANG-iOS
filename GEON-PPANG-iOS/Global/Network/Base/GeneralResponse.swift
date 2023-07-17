bc7e4fcab0aad6f3488a41c0fc9cb0991570d27b
jeongdung-eo <jeuniis2@gmail.com>
2023-07-18 04:19:31 +0900

    [Fix] #62 - network generic  수정

--- GEON-PPANG-iOS/Global/Network/Base/GeneralResponse.swift
+++ GEON-PPANG-iOS/Global/Network/Base/GeneralResponse.swift
@@ -8,46 +8,40 @@
 import Foundation
 
 struct GeneralResponse<T: Decodable>: Decodable {
-    var status: Int
-    var success: Bool
+    var code: Int
     var message: String?
     var data: T?
     
     enum CodingKeys: String, CodingKey {
         case message
         case data
-        case status
-        case success
+        case code
     }
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         message = (try? values.decode(String.self, forKey: .message)) ?? ""
         data = (try? values.decode(T.self, forKey: .data)) ?? nil
-        status = (try? values.decode(Int.self, forKey: .status)) ?? 0
-        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
+        code = (try? values.decode(Int.self, forKey: .code)) ?? 0
     }
 }
 
 struct GeneralArrayResponse<T: Decodable>: Decodable {
-    let status: Int
+    let code: Int
     let message: String?
-    let success: Bool?
     let data: [T]?
     
     enum CodingKeys: String, CodingKey {
         case message
         case data
-        case status
-        case success
+        case code
     }
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         message = (try? values.decode(String.self, forKey: .message)) ?? ""
         data = (try? values.decode([T].self, forKey: .data)) ?? []
-        status = (try? values.decode(Int.self, forKey: .status)) ?? 0
-        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
+        code = (try? values.decode(Int.self, forKey: .code)) ?? 0
     }
 }
 
