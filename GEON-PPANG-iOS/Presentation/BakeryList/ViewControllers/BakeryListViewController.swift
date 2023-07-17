b5fbebb9c88d7bad78b27040335e1b663b8886c6
seongmin221 <seongmin221@naver.com>
2023-07-18 03:31:45 +0900

    [Fix] #55 - review applied

--- GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/BakeryListViewController.swift
+++ GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/BakeryListViewController.swift
@@ -40,10 +40,6 @@ final class BakeryListViewController: BaseViewController {
         
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-    
     // MARK: - Setting
     
     override func setUI() {
