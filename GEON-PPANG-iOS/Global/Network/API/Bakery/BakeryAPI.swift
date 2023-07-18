c61216bd1b7ef66b5b56b45fe546cb45e01d898a
seongmin221 <seongmin221@naver.com>
2023-07-19 04:25:50 +0900

    [Feat] #74 - WriteReviewAPI service added

--- GEON-PPANG-iOS/Global/Network/API/Bakery/BakeryAPI.swift
+++ GEON-PPANG-iOS/Global/Network/API/Bakery/BakeryAPI.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+import Moya
+
+final class BakeryAPI {
+    
+    typealias WriteReviewResponse = GeneralResponse<WriteReviewDTO>
+    
+    static let shared: BakeryAPI = BakeryAPI()
+    
+    private init() {}
+    
+    var bakeryProvider = MoyaProvider<BakeryService>(plugins: [MoyaLoggingPlugin()])
+    
+    public private(set) var writeReview: WriteReviewResponse?
+    
+    // MARK: - POST
+    
+    func writeReview(bakeryID: String, completion: @escaping (WriteReviewResponse?) -> Void) {
+        bakeryProvider.request(.writeReview(bakeryID: bakeryID)) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    self.writeReview = try response.map(WriteReviewResponse.self)
+                    guard let writeReview = self.writeReview else { return }
+                    
+                    completion(writeReview)
+                    print("✅\(writeReview)")
+                } catch let err {
+                    print(err.localizedDescription, 500)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+}
--- GEON-PPANG-iOS/Global/Network/Service/Bakery/BakeryService.swift
+++ GEON-PPANG-iOS/Global/Network/Service/Bakery/BakeryService.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+import Moya
+
+enum BakeryService {
+    case writeReview(bakeryID: String)
+}
+
+extension BakeryService: TargetType {
+    var baseURL: URL {
+        return URL(string: Config.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .writeReview:
+            return URLConstant.writeReview
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .writeReview:
+            return .post
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .writeReview(bakeryID: let bakeryID):
+            return .requestParameters(parameters: ["bakeryID": bakeryID], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String: String]? {
+        switch self {
+        default:
+            return NetworkConstant.hasTokenHeader
+        }
+    }
+}
