78d12aaf11d3ca6887a1c068203aedf21e2ba7b6
jeongdung-eo <jeuniis2@gmail.com>
2023-07-19 00:51:02 +0900

    [Fix] #71 - bakeryList DTO 변경

--- GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/Model/BakeryListResponseDTO.swift
+++ GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/Model/BakeryListResponseDTO.swift
@@ -5,9 +5,12 @@
 //  Created by JEONGEUN KIM on 2023/07/11.
 //
 
+import Foundation
+
 // MARK: - BakeryListResponseDTO
 
 struct BakeryListResponseDTO: Codable, Hashable, BakeryListProtocol {
+
     let bakeryID: Int
     let bakeryName: String
     let bakeryPicture: String
@@ -19,7 +22,26 @@ struct BakeryListResponseDTO: Codable, Hashable, BakeryListProtocol {
     let secondNearStation: String?
     let isBooked: Bool
     let bookMarkCount: Int
+    let reviewCount: Int
     
+    enum CodingKeys: String, CodingKey {
+        case bakeryID = "bakeryId"
+        case bakeryName
+        case bakeryPicture
+        case isHACCP
+        case isVegan
+        case isNonGMO
+        case breadType
+        case firstNearStation
+        case secondNearStation
+        case isBooked = "isBookMarked"
+        case bookMarkCount
+        case reviewCount
+    }
+    
+    func convertToBakeryList() -> BakeryList {
+        return BakeryList(bakeryID: bakeryID, bakeryName: bakeryName, bakeryPicture: bakeryPicture, isHACCP: isHACCP, isVegan: isVegan, isNonGMO: isNonGMO, breadType: breadType, firstNearStation: firstNearStation, secondNearStation: secondNearStation, isBooked: isBooked, bookMarkCount: bookMarkCount, reviewCount: reviewCount)
+    }
 }
 
 // MARK: - BreadType
@@ -32,6 +54,15 @@ struct BreadResponseType: Codable, Hashable {
     let isNutFree: Bool
     let isSugarFree: Bool
     
+    enum CodingKeys: String, CodingKey {
+        case breadTypeID = "breadTypeId"
+        case breadTypeName
+        case isGlutenFree
+        case isVegan
+        case isNutFree
+        case isSugarFree
+    }
+    
     func configureTrueOptions() -> [(String, Bool)] {
         var optionsBoolArray: [(String, Bool)] = []
         if isGlutenFree { optionsBoolArray.append((I18N.BakeryList.glutenfree, true)) }
@@ -40,4 +71,31 @@ struct BreadResponseType: Codable, Hashable {
         if isSugarFree { optionsBoolArray.append((I18N.BakeryList.noSugar, true)) }
         return optionsBoolArray
     }
+}
+
+// BakeryList
+
+struct BakeryList: Codable, Hashable, BakeryListProtocol {
+    var id = UUID()
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: BakeryList, rhs: BakeryList) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    let bakeryID: Int
+    let bakeryName: String
+    let bakeryPicture: String
+    let isHACCP: Bool
+    let isVegan: Bool
+    let isNonGMO: Bool
+    let breadType: BreadResponseType
+    let firstNearStation: String
+    let secondNearStation: String?
+    let isBooked: Bool
+    let bookMarkCount: Int
+    let reviewCount: Int
+    
 }
