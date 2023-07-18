e6c99bcb88ca69742e6900ed5e88327049b668ba
jeongdung-eo <jeuniis2@gmail.com>
2023-07-18 15:54:28 +0900

    [Fix] #66 - Home관련 DTO에 convert 함수 추가

--- GEON-PPANG-iOS/Global/Network/DataModel/Home/HomeBestBakeryResponseDTO.swift
+++ GEON-PPANG-iOS/Global/Network/DataModel/Home/HomeBestBakeryResponseDTO.swift
@@ -21,4 +21,33 @@ struct HomeBestBakeryResponseDTO: Codable, Hashable {
     let bookMarkCount: Int
     let reviewCount: Int
     
+    func convertToBestBakery() -> BestBakery {
+        return BestBakery(bakeryId: bakeryId, bakeryName: bakeryName, bakeryPicture: bakeryPicture, isHACCP: isHACCP, isVegan: isVegan, isNonGMO: isNonGMO, firstNearStation: firstNearStation, secondNearStation: secondNearStation, isBookMarked: isBookMarked, bookMarkCount: bookMarkCount, reviewCount: reviewCount)
+    }
+    
+}
+
+
+struct BestBakery: Codable, Hashable {
+    var id = UUID()
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: BestBakery, rhs: BestBakery) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    let bakeryId: Int
+    let bakeryName: String
+    let bakeryPicture: String
+    let isHACCP: Bool
+    let isVegan: Bool
+    let isNonGMO: Bool
+    let firstNearStation: String
+    let secondNearStation: String?
+    let isBookMarked: Bool
+    let bookMarkCount: Int
+    let reviewCount: Int
+    
 }
--- GEON-PPANG-iOS/Global/Network/DataModel/Home/HomeBestReviewResponseDTO.swift
+++ GEON-PPANG-iOS/Global/Network/DataModel/Home/HomeBestReviewResponseDTO.swift
@@ -10,15 +10,8 @@ import Foundation
 // MARK: - HomeBestReviewResponseDTO
 
 struct HomeBestReviewResponseDTO: Codable, Hashable {
-    var id = UUID()
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
     
-    static func == (lhs: HomeBestReviewResponseDTO, rhs: HomeBestReviewResponseDTO) -> Bool {
-        lhs.id == rhs.id
-    }
-    var bakeryId: Int
+    let bakeryId: Int
     let bakeryName: String
     let isHACCP: Bool
     let isVegan: Bool
@@ -32,4 +25,35 @@ struct HomeBestReviewResponseDTO: Codable, Hashable {
     let reviewText: String
     let firstMaxRecommendKeyword: String
     let secondMaxRecommendKeyword: String?
+    
+    func convertToBakeryReviews() -> BestReviews {
+        return BestReviews(bakeryId: bakeryId, bakeryName: bakeryName, isHACCP: isHACCP, isVegan: isVegan, isNonGMO: isNonGMO, firstNearStation: firstNearStation, secondNearStation: secondNearStation, isBookMarked: isBookMarked, bookMarkCount: bookMarkCount, bakeryPicture: bakeryPicture, reviewCount: reviewCount, reviewText: reviewText, firstMaxRecommendKeyword: firstMaxRecommendKeyword, secondMaxRecommendKeyword: secondMaxRecommendKeyword)
+    }
+}
+
+struct BestReviews: Codable, Hashable {
+    var id = UUID()
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: BestReviews, rhs: BestReviews) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    let bakeryId: Int
+    let bakeryName: String
+    let isHACCP: Bool
+    let isVegan: Bool
+    let isNonGMO: Bool
+    let firstNearStation: String
+    let secondNearStation: String?
+    let isBookMarked: Bool
+    let bookMarkCount: Int
+    let bakeryPicture: String
+    let reviewCount: Int
+    let reviewText: String
+    let firstMaxRecommendKeyword: String
+    let secondMaxRecommendKeyword: String?
+
 }
