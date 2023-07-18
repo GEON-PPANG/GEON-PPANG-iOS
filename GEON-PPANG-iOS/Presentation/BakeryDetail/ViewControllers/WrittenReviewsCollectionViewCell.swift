ab06b1fc2cf81601138a3de68f328291e3156eb1
ckkim817 <ckkim817@konkuk.ac.kr>
2023-07-18 16:57:47 +0900

    [Add] #45 - 작성된리뷰셀 생성

---
+++ GEON-PPANG-iOS/Presentation/BakeryDetail/ViewControllers/WrittenReviewsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WrittenReviewsCollectionViewCell.swift
+//  GEON-PPANG-iOS
+//
+//  Created by kyun on 2023/07/18.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class WrittenReviewsCollectionViewCell: UICollectionViewCell {
+    
+    
+}
