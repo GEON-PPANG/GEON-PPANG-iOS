ba60f73d00782d8303401d3ce1c1d103a2436929
jeongdung-eo <jeuniis2@gmail.com>
2023-07-19 11:45:00 +0900

    [Fix] #76 - Response DTO 수정

--- GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/Model/BakeryListResponseDTO.swift
+++ GEON-PPANG-iOS/Presentation/BakeryList/ViewControllers/Model/BakeryListResponseDTO.swift
@@ -5,10 +5,12 @@
 //  Created by JEONGEUN KIM on 2023/07/11.
 //
 
+import Foundation
+
 // MARK: - BakeryListResponseDTO
 
-struct BakeryListResponseDTO: Hashable, BakeryListProtocol {
-    var reviewCount: Int
+struct BakeryListResponseDTO: Codable, Hashable, BakeryListProtocol {
+    let reviewCount: Int
     let bakeryId: Int
     let bakeryName: String
     let bakeryPicture: String
@@ -20,6 +22,10 @@ struct BakeryListResponseDTO: Hashable, BakeryListProtocol {
     let secondNearStation: String?
     let isBookMarked: Bool
     let bookMarkCount: Int
+    
+    func convertToBakeryList() -> BakeryList {
+        return BakeryList(reviewCount: reviewCount, bakeryId: bakeryId, bakeryName: bakeryName, bakeryPicture: bakeryPicture, isHACCP: isHACCP, isVegan: isVegan, isNonGMO: isNonGMO, breadType: breadType, firstNearStation: firstNearStation, secondNearStation: secondNearStation, isBookMarked: isBookMarked, bookMarkCount: bookMarkCount)
+    }
 }
 
 // MARK: - BreadType
@@ -40,4 +46,30 @@ struct BreadResponseType: Codable, Hashable {
         if isSugarFree { optionsBoolArray.append((I18N.BakeryList.noSugar, true)) }
         return optionsBoolArray
     }
+}
+
+struct BakeryList: Codable, Hashable, BakeryListProtocol {
+    
+    var id = UUID()
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: BakeryList, rhs: BakeryList) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    let reviewCount: Int
+    let bakeryId: Int
+    let bakeryName: String
+    let bakeryPicture: String
+    let isHACCP: Bool
+    let isVegan: Bool
+    let isNonGMO: Bool
+    let breadType: BreadResponseType
+    let firstNearStation: String
+    let secondNearStation: String?
+    let isBookMarked: Bool
+    let bookMarkCount: Int
+    
 }
