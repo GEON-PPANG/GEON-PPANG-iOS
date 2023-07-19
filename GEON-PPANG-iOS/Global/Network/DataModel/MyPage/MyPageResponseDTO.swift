f0f621e9fb98a24ec36fdba8dc42d522ca6a3ef5
seongmin221 <seongmin221@naver.com>
2023-07-19 19:58:14 +0900

    [Chore] #80 - MyPageResponse name edited

--- GEON-PPANG-iOS/Global/Network/DataModel/MyPage/MyPageResponseDTO.swift
+++ GEON-PPANG-iOS/Global/Network/DataModel/MyPage/MyPageResponseDTO.swift
@@ -2,7 +2,31 @@
 //  MyPageResponseDTO.swift
 //  GEON-PPANG-iOS
 //
-//  Created by JEONGEUN KIM on 2023/07/17.
+//  Created by 이성민 on 2023/07/14.
 //
 
 import Foundation
+
+struct MyPageResponseDTO: Codable {
+    let memberNickname: String
+    let mainPurpose: String
+    let breadType: BreadResponseType
+}
+
+extension MyPageResponseDTO {
+    
+    static func dummyData() -> MyPageResponseDTO {
+        return .init(memberNickname: "Id",
+                     mainPurpose: "HEALTH",
+                     breadType: .init(breadTypeID: 1,
+                                      name: "맛 • 다이어트",
+                                      isGlutenFree: true,
+                                      isVegan: true,
+                                      isNutFree: false,
+                                      isSugarFree: true))
+    }
+    
+    static func emptyData() -> MyPageResponseDTO {
+        return .init(memberNickname: "", mainPurpose: "", breadType: .emptyBreadType())
+    }
+}
