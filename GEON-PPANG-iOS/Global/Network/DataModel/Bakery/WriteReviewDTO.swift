ae117cbe8e0c6465db996bc9dfdc239cbc40d4f1
seongmin221 <seongmin221@naver.com>
2023-07-20 00:08:02 +0900

    [Feat] #74 - DTO edited

--- GEON-PPANG-iOS/Global/Network/DataModel/Bakery/WriteReviewDTO.swift
+++ GEON-PPANG-iOS/Global/Network/DataModel/Bakery/WriteReviewDTO.swift
@@ -10,7 +10,7 @@ import Foundation
 struct WriteReviewDTO: Codable {
     let bakeryID: Int
     var isLike: Bool
-    var keywordList: [String]
+    var keywordList: [SingleKeyword]
     var reviewText: String
 
     enum CodingKeys: String, CodingKey {
@@ -21,11 +21,22 @@ struct WriteReviewDTO: Codable {
     }
 }
 
-struct KeywordList: Codable {
+struct SingleKeyword: Codable, Equatable {
+    let keywordName: String
+}
+
+struct KeywordDescriptionList: Codable {
     enum Keyword: String, CaseIterable {
         case delicious = "맛있어요"
         case kind = "친절해요"
         case special = "특별한 메뉴"
         case zeroWaste = "제로 웨이스트"
+    }
+    
+    enum Request: String, CaseIterable {
+        case delicious = "DELICIOUS"
+        case kind = "KIND"
+        case special = "SPECIAL_MENU"
+        case zeroWaste = "ZERO_WASTE"
     }
 }
