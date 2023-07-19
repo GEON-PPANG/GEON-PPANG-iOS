ed0dbde664ae089bfda78168b08abdcaa5858520
jeongdung-eo <jeuniis2@gmail.com>
2023-07-19 18:39:50 +0900

    [Feat] #72 - 내가 작성한 리뷰 API 구현

--- GEON-PPANG-iOS/Global/Network/API/MyPage/MyPageAPI.swift
+++ GEON-PPANG-iOS/Global/Network/API/MyPage/MyPageAPI.swift
@@ -11,22 +11,25 @@ import Moya
 
 final class MyPageAPI {
     
+    typealias Bookmarks = GeneralArrayResponse<BakeryListResponseDTO>
+  //  typealias Myreviews = GeneralArrayResponse<m
+    
     static let shared: MyPageAPI = MyPageAPI()
     
     private init() { }
     
     var MyPageProvider = MoyaProvider<MyPageService>(plugins: [MoyaLoggingPlugin()])
     
-    public private(set) var bookmarks: GeneralArrayResponse<BakeryListResponseDTO>?
+    public private(set) var bookmarks: Bookmarks?
     
     // MARK: - GET
     
-    func getBookmarks(completion: @escaping (GeneralArrayResponse<BakeryListResponseDTO>?) -> Void) {
+    func getBookmarks(completion: @escaping (Bookmarks?) -> Void) {
         MyPageProvider.request(.bookmarks) { result in
             switch result {
             case let .success(response):
                 do {
-                    let data = try response.map(GeneralArrayResponse<BakeryListResponseDTO>.self)
+                    let data = try response.map(Bookmarks.self)
                     completion(data)
                     print("✅\(data)")
                 } catch let err {
