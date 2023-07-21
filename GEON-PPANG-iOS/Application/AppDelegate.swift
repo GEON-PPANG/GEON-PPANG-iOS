35a4d59d88bce2934bd48cbc36cee1f235804d24
ckkim817 <ckkim817@konkuk.ac.kr>
2023-07-21 14:14:26 +0900

    [Feat] #99 - 온보딩 뷰 레이아웃 기기대응 추가

--- GEON-PPANG-iOS/Application/AppDelegate.swift
+++ GEON-PPANG-iOS/Application/AppDelegate.swift
@@ -12,7 +12,6 @@ import FirebaseCore
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         sleep(UInt32(1.5))
@@ -33,7 +32,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
