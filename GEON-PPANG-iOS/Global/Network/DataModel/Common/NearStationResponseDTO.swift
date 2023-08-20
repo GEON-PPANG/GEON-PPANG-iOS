5886972dd8e73f00d0a4791f5e05f931a92cf799
jeongdung-eo <jeuniis2@gmail.com>
2023-08-21 02:59:48 +0900

    [Feat] #119 - near station DTO 구현

---
+++ GEON-PPANG-iOS/Global/Network/DataModel/Common/NearStationResponseDTO.swift
@@ -0,0 +1,15 @@
+//
+//  NearStationResponseDTO.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/08/21.
+//
+
+import Foundation
+
+struct NearStationResponseDTO: Hashable, Codable {
+    
+    let firstNearStation: String
+    let secondNearStation: String?
+    
+}
