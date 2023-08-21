ad2d8c58123d7ae0faf98fcb7a59b311942fcf08
jeongdung-eo <jeuniis2@gmail.com>
2023-08-21 13:51:41 +0900

    [Feat] #119 - 홈 VC 로직 구현

--- GEON-PPANG-iOS/Presentation/Home/ViewControllers/HomeViewController.swift
+++ GEON-PPANG-iOS/Presentation/Home/ViewControllers/HomeViewController.swift
@@ -21,26 +21,27 @@ enum Sections: Int, Hashable, CaseIterable {
         }
     }
 }
+
+enum Item: Hashable {
+    
+    case bakery(HomeBestBakeryResponseDTO)
+    case reviews(HomeBestReviewResponseDTO)
+    case bottom
+
+}
+
 final class HomeViewController: BaseViewController {
     
     // MARK: - Property
+
+    typealias DataSource = UICollectionViewDiffableDataSource<Sections, Item>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Sections, Item>
     
-    typealias DataSource = UICollectionViewDiffableDataSource<Sections, AnyHashable>
     private var dataSource: DataSource?
     
-    private var bakeryList: [HomeBestBakeryResponseDTO] = [] {
-        didSet {
-            self.setReloadData()
-        }
-    }
-
-    private var reviewList: [HomeBestReviewResponseDTO] = [] {
-        didSet {
-            self.setReloadData()
-        }
-    }
+    private var bakeryList: [HomeBestBakeryResponseDTO] = []
+    private var reviewList: [HomeBestReviewResponseDTO] = []
     
-    private lazy var safeArea = self.view.safeAreaLayoutGuide
     private var nickname =  UserDefaults.standard.string(forKey: "nickname") ?? ""
     
     // MARK: - UI Property
@@ -61,7 +62,7 @@ final class HomeViewController: BaseViewController {
         
         setRegistration()
         setDataSource()
-        setReloadData()
+        setSupplementaryView()
     }
     
     // MARK: - Setting
@@ -101,56 +102,59 @@ final class HomeViewController: BaseViewController {
         
         collectionView.do {
             $0.showsVerticalScrollIndicator = false
-            $0.backgroundColor = .white
-            $0.bounces = false
+            $0.backgroundColor = .gbbBackground1
             $0.delegate = self
         }
     }
         
     private func setRegistration() {
         
-        collectionView.register(cell: HomeBakeryCollectionViewCell.self)
-        collectionView.register(cell: HomeReviewCollectionViewCell.self)
-        collectionView.register(cell: HomeBottomCollectionViewCell.self)
+        collectionView.registerCells(cells: [HomeBakeryCollectionViewCell.self,
+                                             HomeReviewCollectionViewCell.self,
+                                             HomeBottomCollectionViewCell.self])
         collectionView.register(header: HomeHeaderView.self)
     }
     
     private func setDataSource() {
         
         dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
-            let section = self.dataSource?.snapshot().sectionIdentifiers[indexPath.section]
-            switch section {
-            case .bakery:
+            
+            switch item {
+            case .bakery(let data):
                 let cell: HomeBakeryCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
-                cell.configureCellUI(data: item as! HomeBestBakeryResponseDTO)
+                cell.configureCellUI(data: data)
                 return cell
-            case .review:
+            case .reviews(let data):
                 let cell: HomeReviewCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
-                cell.configureCellUI(data: item as! HomeBestReviewResponseDTO)
+                cell.configureCellUI(data: data)
                 return cell
-            case .bottom, .none:
+            case .bottom:
                 let cell: HomeBottomCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
                 return cell
             }
         })
     }
     
-    private func setReloadData() {
-        
-        var snapshot = NSDiffableDataSourceSnapshot<Sections, AnyHashable>()
+    private func setSnapshot() {
+
+        var snapshot = Snapshot()
         defer { dataSource?.apply(snapshot, animatingDifferences: false)}
+
+        snapshot.appendSections(Sections.allCases)
         
-        snapshot.appendSections([.bakery, .review, .bottom])
-        
-        let itemsDictionary: [Sections: [AnyHashable]] = [
-            .bakery: bakeryList,
-            .review: reviewList,
-            .bottom: [0]
+        let itemsDictionary: [Sections: [Item]] = [
+            .bakery: bakeryList.map { .bakery($0) },
+            .review: reviewList.map { .reviews($0) },
+            .bottom: [.bottom]
         ]
-        
+
         for (section, items) in itemsDictionary {
             snapshot.appendItems(items, toSection: section)
         }
+
+   }
+    
+    private func setSupplementaryView() {
         
         dataSource?.supplementaryViewProvider = { (collectionView, _, indexPath) in
             let header: HomeHeaderView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, indexPath: indexPath)
@@ -168,42 +172,55 @@ final class HomeViewController: BaseViewController {
         
     }
     
-    func layout() -> UICollectionViewLayout {
-        let layout = UICollectionViewCompositionalLayout { (sectionIndex, _) -> NSCollectionLayoutSection? in
-            let section = Sections(rawValue: sectionIndex)!
-            switch section {
-            case .bakery:
-                return self.normalSection(headerSize: 49)
-            case .review:
-                return self.normalSection(headerSize: 25)
-            case .bottom:
-                return self.bottomSection()
+    func layout() -> UICollectionViewCompositionalLayout {
+        
+        return UICollectionViewCompositionalLayout(sectionProvider: { [weak self] sectionIndex, _ in
+            switch sectionIndex {
+            case 0:
+                return self?.normalSection(headerSize: 49)
+            case 1:
+                return self?.normalSection(headerSize: 25)
+            default:
+                return self?.bottomSection()
             }
-        }
-        return layout
+        })
     }
     
     private func normalSection(headerSize: CGFloat) -> NSCollectionLayoutSection {
+        
         let itemGroupSize = NSCollectionLayoutSize(widthDimension: .absolute(convertByWidthRatio(192)),
                                                    heightDimension: .estimated(convertByHeightRatio(236)))
         let item = NSCollectionLayoutItem(layoutSize: itemGroupSize)
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: itemGroupSize, subitems: [item])
+        
         let section = NSCollectionLayoutSection(group: group)
         section.interGroupSpacing = 12
         section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
-        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(headerSize))
-        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 24, leading: 24, bottom: 30, trailing: 24)
+        
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                heightDimension: .absolute(headerSize))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize,
+                                                                 elementKind: UICollectionView.elementKindSectionHeader,
+                                                                 alignment: .top)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 24,
+                                                        leading: 24,
+                                                        bottom: 30,
+                                                        trailing: 24)
         section.boundarySupplementaryItems = [header]
         return section
     }
     
     private func bottomSection() -> NSCollectionLayoutSection {
+        
         let itemGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
                                                    heightDimension: .absolute(convertByHeightRatio(115)))
         let item = NSCollectionLayoutItem(layoutSize: itemGroupSize)
-        let group = NSCollectionLayoutGroup.vertical(layoutSize: itemGroupSize, subitem: item, count: 1)
+        
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: itemGroupSize,
+                                                     subitem: item,
+                                                     count: 1)
         let section = NSCollectionLayoutSection(group: group)
+        
         return section
     }
 }
@@ -211,37 +228,49 @@ final class HomeViewController: BaseViewController {
 // MARK: - UICollectionViewDelegate
 
 extension HomeViewController: UICollectionViewDelegate {
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
         let nextViewController = BakeryDetailViewController()
-        let section = Sections(rawValue: indexPath.section)!
+        
+        let section = dataSource?.sectionIdentifier(for: indexPath.section)
         switch section {
         case .bakery:
             nextViewController.bakeryID = self.bakeryList[indexPath.item].bakeries.bakeryID
+            Utils.push(self.navigationController, nextViewController)
+
         case .review:
             nextViewController.bakeryID = self.reviewList[indexPath.item].reviews.bakeryID
-        case .bottom:
-            return
+            Utils.push(self.navigationController, nextViewController)
+
+        default:
+            break
         }
-        Utils.push(self.navigationController, nextViewController)
+        
     }
 }
 
 // MARK: - API
 
 extension HomeViewController {
+    
     private func getHomeBestData() {
+        
         HomeAPI.shared.getBestBakery { response in
             guard let response = response else { return }
             guard let data = response.data else { return }
-            var bakeryList = data.map { $0 }
+            let bakeryList = data.map { $0 }
             self.bakeryList = bakeryList
+            self.setSnapshot()
         }
         
         HomeAPI.shared.getBestReviews { response in
             guard let response = response else { return }
             guard let data = response.data else { return }
-            var reviewsList = data.map { $0 }
+            let reviewsList = data.map { $0 }
             self.reviewList = reviewsList
+            self.setSnapshot()
+
         }
     }
 }
