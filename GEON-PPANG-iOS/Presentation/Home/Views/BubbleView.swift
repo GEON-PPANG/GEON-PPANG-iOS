aabf328d3ddfb6b115d6993608a0017381a84540
jeongdung-eo <jeuniis2@gmail.com>
2023-08-29 00:26:27 +0900

    [Add] #145 - bubble icon 추가

---
+++ GEON-PPANG-iOS/Presentation/Home/Views/BubbleView.swift
@@ -0,0 +1,20 @@
+//
+//  BubbleView.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/08/28.
+//
+
+import UIKit
+
+class BubbleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
