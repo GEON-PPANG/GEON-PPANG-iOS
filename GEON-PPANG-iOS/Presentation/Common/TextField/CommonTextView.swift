faf931262b57ee4250d4ac528f8eedb6aa9a865f
jeongdung-eo <jeuniis2@gmail.com>
2023-08-28 19:04:08 +0900

    [Add] #141 - 로그인관련 로직 추가

--- GEON-PPANG-iOS/Presentation/Common/TextField/CommonTextView.swift
+++ GEON-PPANG-iOS/Presentation/Common/TextField/CommonTextView.swift
@@ -11,15 +11,17 @@ import SnapKit
 import Then
 
 enum SignInPropetyType: String, CaseIterable {
-    case email = "이메일"
-    case password = "비밀번호"
+    
+    case email, loginEmail = "이메일"
+    case password, loginPassword = "비밀번호"
     case checkPassword = "비밀번호 재확인"
     case nickname = "닉네임"
     
     var placeHolder: String {
         switch self {
-        case .email: return  "이메일을 입력해주세요"
+        case .email, .loginEmail: return  "이메일을 입력해주세요"
         case .password: return "영문, 숫자 포함 8자리이상"
+        case .loginPassword: return "비밀번호를 입력해주세요"
         case .checkPassword: return "비밀번호를 재입력해주세요"
         case .nickname: return "닉네임 10자 이내, 특수문자 금지"
         }
@@ -155,6 +157,8 @@ extension CommonTextView: UITextFieldDelegate {
             }
         case .checkPassword:
             self.textFieldData?(fetchText())
+        case .loginPassword, .loginEmail:
+             return
         }
         
         if text.isEmpty {
