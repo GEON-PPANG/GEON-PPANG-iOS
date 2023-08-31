a9abe061a18eb4cd9c13d238fb8a782ab8008858
seongmin221 <seongmin221@naver.com>
2023-08-31 17:49:18 +0900

    [Add] #132 - added WebViewController & UrlLiteral

---
+++ GEON-PPANG-iOS/Presentation/Common/WebView/WebViewController.swift
@@ -0,0 +1,71 @@
+//
+//  WebViewController.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 2023/08/31.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewController: BaseViewController {
+    
+    // MARK: - Property
+    
+    var url: String
+    
+    // MARK: - UI Property
+    
+    private let navigationBar = CustomNavigationBar()
+    private var webView = WKWebView()
+    
+    // MARK: - Life Cycle
+    
+    init(url: String) {
+        self.url = url
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setting
+    
+    override func setLayout() {
+        
+        view.addSubview(navigationBar)
+        navigationBar.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
+        }
+        
+        view.addSubview(webView)
+        webView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
+    
+    override func setUI() {
+        
+        navigationBar.do {
+            $0.configureBottomLine()
+            $0.configureCenterTitle(to: "이용약관")
+            $0.configureBackButtonAction(popViewControllerAction())
+        }
+        
+        setWebViewURL()
+    }
+    
+    private func setWebViewURL() {
+
+        guard let url = URL(string: self.url)
+        else { return }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    
+}
