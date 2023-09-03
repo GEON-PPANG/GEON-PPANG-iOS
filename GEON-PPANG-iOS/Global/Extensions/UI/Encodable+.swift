c9d43f86cbfc7bb0d85a7fa838ca76dd5dcc795a
jeongdung-eo <jeuniis2@gmail.com>
2023-09-03 13:15:47 +0900

    [Feat] #153 - encodable extension 추가

---
+++ GEON-PPANG-iOS/Global/Extensions/UI/Encodable+.swift
@@ -0,0 +1,21 @@
+//
+//  Encodable+.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/09/03.
+//
+
+import Foundation
+
+extension Encodable {
+    
+  func asParameter() throws -> [String: Any] {
+    let data = try JSONEncoder().encode(self)
+    guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            as? [String: Any] else {
+      throw NSError()
+    }
+    
+    return dictionary
+  }
+}
