6f5aaf005c0b690f299b98f1b97862ce2fbf421b
ckkim817 <ckkim817@konkuk.ac.kr>
2023-09-07 14:27:44 +0900

    [Fix] #148 - 코드 리뷰 반영 1
    string 리터럴로 빼고 불필요한 코드 삭제

--- GEON-PPANG-iOS/Presentation/BakeryDetail/Views/BakeryDetailCollectionViewFooter.swift
+++ GEON-PPANG-iOS/Presentation/BakeryDetail/Views/BakeryDetailCollectionViewFooter.swift
@@ -71,7 +71,7 @@ final class BakeryDetailCollectionViewFooter: UICollectionReusableView {
         }
         
         label.do {
-            $0.basic(text: "가게 상세정보 및 메뉴는 업체에서 제공한 정보를 바탕으로 합니다.\n건빵집 방문 시 한 번 더 확인하시기를 권장합니다.", font: .captionM2!, color: .gbbGray400!)
+            $0.basic(text: I18N.Detail.menuNotice, font: .captionM2!, color: .gbbGray400!)
             $0.adjustsFontSizeToFitWidth = true
             $0.numberOfLines = 2
         }
