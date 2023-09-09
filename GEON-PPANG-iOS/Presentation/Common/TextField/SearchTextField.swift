7862b5b155231b812db51d64fd14c594e75c06b4
jeongdung-eo <jeuniis2@gmail.com>
2023-09-09 11:48:28 +0900

    [Feat] #168 - textField 패딩 조절

--- GEON-PPANG-iOS/Presentation/Common/TextField/SearchTextField.swift
+++ GEON-PPANG-iOS/Presentation/Common/TextField/SearchTextField.swift
@@ -143,4 +143,23 @@ extension SearchTextField: UITextFieldDelegate {
                                                bottom: 0,
                                                right: -15))
         }
+    
+        override func textRect(forBounds bounds: CGRect) -> CGRect {
+    
+            let rect = super.textRect(forBounds: bounds)
+            return rect.inset(by: UIEdgeInsets(top: 0,
+                                               left: viewType == .home ? 0 : 12,
+                                               bottom: 0,
+                                               right: viewType == .home ? 0 : 15))
+        }
+    
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+    
+            let rect = super.editingRect(forBounds: bounds)
+            return rect.inset(by: UIEdgeInsets(top: 0,
+                                               left: viewType == .home ? 0 : 12,
+                                               bottom: 0,
+                                               right: viewType == .home ? 0 :15))
+        }
+
 }
