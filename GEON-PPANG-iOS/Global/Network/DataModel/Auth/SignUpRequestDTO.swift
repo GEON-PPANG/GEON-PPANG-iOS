10d4970f85eb67f8b8b112b732a8ec4bef6ee8da
seongmin221 <seongmin221@naver.com>
2023-09-09 18:22:16 +0900

    [Add] #171 - added platformenum and signuprequestDTO

---
+++ GEON-PPANG-iOS/Global/Network/DataModel/Auth/SignUpRequestDTO.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpRequestDTO.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 2023/09/09.
+//
+
+import Foundation
+
+struct SignUpRequestDTO: Codable {
+    let platformType: String
+    let email: String
+    let password: String
+    let nickname: String
+}
