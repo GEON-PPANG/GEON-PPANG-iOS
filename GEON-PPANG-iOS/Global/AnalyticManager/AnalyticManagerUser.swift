783caa5de8f736888f68f32fca766185967ab656
jeongdung-eo <jeuniis2@gmail.com>
2023-09-15 11:32:05 +0900

    [Feat] #178 - Analytic User Property Enum 구현

---
+++ GEON-PPANG-iOS/Global/AnalyticManager/AnalyticManagerUser.swift
@@ -0,0 +1,34 @@
+//
+//  AnalyticManagerUser.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/09/15.
+//
+
+import Foundation
+
+
+enum AnalyticManagerUser: AnalyticManagerEventProtocol {
+    
+    case authType
+    case accountCreationDate
+    case mainPurpose
+    case breadType
+    case ingredientsType
+    case totalReview
+    case totalFavorites
+    case totalReviewReport
+    
+    var name: String {
+        switch self {
+        case .authType: return "auth type"
+        case .accountCreationDate: return "account creation date"
+        case .mainPurpose: return "main purpose"
+        case .breadType: return "bread type"
+        case .ingredientsType: return "ingredients type"
+        case .totalReview: return "total review"
+        case .totalFavorites: return "total favorites"
+        case .totalReviewReport: return "total review report"
+        }
+    }    
+}
