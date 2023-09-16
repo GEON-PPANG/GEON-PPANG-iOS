748f1cfe8d41aa6eaa4c693f8a29641bae553e1f
jeongdung-eo <jeuniis2@gmail.com>
2023-09-17 02:05:50 +0900

    [Feat] #185 - Analytic Event Enum 추가

---
+++ GEON-PPANG-iOS/Global/Enum/AnalyticEventType.swift
@@ -0,0 +1,25 @@
+//
+//  AnalyticEventType.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 2023/09/17.
+//
+
+import Foundation
+
+enum AnalyticEventType: String {
+    
+    /// Auth
+    
+    case KAKAO
+    case EMAIL
+    case APPLE
+
+    /// detilpage source
+   
+    case HOME
+    case SEARCH
+    case LIST
+    case MYPAGE_MYSTORE
+    case MYPAGE_MYREVIEW
+}
