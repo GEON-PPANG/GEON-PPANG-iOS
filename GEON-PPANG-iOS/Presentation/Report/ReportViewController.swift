c20fe3ee74ea2bbf857944d7915c0ca2d1befb81
ckkim817 <ckkim817@konkuk.ac.kr>
2023-09-18 10:59:21 +0900

    [Feat] #183 - 브랜치 변경을 위한 임시 커밋

---
+++ GEON-PPANG-iOS/Presentation/Report/ReportViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ReportViewController.swift
+//  GEON-PPANG-iOS
+//
+//  Created by kyun on 2023/09/17.
+//
+
+import UIKit
+
+final class ReportViewController: BaseViewController {
+    
+    // MARK: - UI Property
+    
+    private let navigationBar = CustomNavigationBar()
+    
+    
+    // MARK: - Setting
+}
