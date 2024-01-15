f68fe73c398c23b16a1043cad85ea9e9ffa255ce
seongmin221 <seongmin221@naver.com>
2024-01-16 00:00:16 +0900

    [Refact] #227 - reviews service refact

--- GEON-PPANG-iOS/Global/Network/NewService/ReviewsService.swift
+++ GEON-PPANG-iOS/Global/Network/NewService/ReviewsService.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+import Moya
+
+enum ReviewsService {
+    case writeReview(bakeryID: Int, content: WriteReviewRequestDTO)
+    case fetchMyReviewDetail(reviewID: Int)
+}
+
+extension ReviewsService: GBService {
+    var domain: GBDomain {
+        return .reviews
+    }
+    
+    var urlPath: String {
+        switch self {
+        case .writeReview(bakeryID: let bakeryID, _):
+            return "\(bakeryID)"
+        case .fetchMyReviewDetail(let reviewID):
+            return "\(reviewID)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .writeReview:
+            return .post
+        case .fetchMyReviewDetail:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .writeReview(_, content: let content):
+            return .requestJSONEncodable(content)
+        case .fetchMyReviewDetail(let reviewID):
+            return .requestPlain
+        }
+    }
+    
+    var headerType: GBHeaderFields {
+        switch self {
+        case .writeReview:
+            return .accessToken
+        case .fetchMyReviewDetail:
+            return .accessToken
+        }
+    }
+    
+}
--- GEON-PPANG-iOS/Global/Network/Service/Review/ReviewService.swift
+++ GEON-PPANG-iOS/Global/Network/Service/Review/ReviewService.swift
@@ -9,51 +9,51 @@ import Foundation
 
 import Moya
 
-enum ReviewService {
-    case writeReview(bakeryID: Int, content: WriteReviewRequestDTO)
-    case fetchMyReviewDetail(reviewID: Int)
-}
-
-extension ReviewService: TargetType {
-    var baseURL: URL {
-        return URL(string: Config.baseURL)!
-    }
-    
-    var path: String {
-        switch self {
-        case .writeReview(bakeryID: let bakeryID, _):
-            return URLConstant.review + "/\(bakeryID)"
-        case .fetchMyReviewDetail(reviewID: let reviewID):
-            return URLConstant.review + "/\(reviewID)"
-        }
-    }
-    
-    var method: Moya.Method {
-        switch self {
-        case .writeReview:
-            return .post
-        case .fetchMyReviewDetail:
-            return .get
-        }
-    }
-    
-    var task: Moya.Task {
-        switch self {
-        case .writeReview(_, content: let content):
-            return .requestJSONEncodable(content)
-        case .fetchMyReviewDetail:
-            return .requestPlain
-        }
-    }
-    
-    var headers: [String: String]? {
-        switch self {
-        default:
-            return NetworkConstant.hasTokenHeader
-        }
-    }
-    
-    var validationType: ValidationType {
-        return .successCodes
-    }
-}
+//enum ReviewsService {
+//    case writeReview(bakeryID: Int, content: WriteReviewRequestDTO)
+//    case fetchMyReviewDetail(reviewID: Int)
+//}
+//
+//extension ReviewsService: TargetType {
+//    var baseURL: URL {
+//        return URL(string: Config.baseURL)!
+//    }
+//    
+//    var path: String {
+//        switch self {
+//        case .writeReview(bakeryID: let bakeryID, _):
+//            return URLConstant.review + "/\(bakeryID)"
+//        case .fetchMyReviewDetail(reviewID: let reviewID):
+//            return URLConstant.review + "/\(reviewID)"
+//        }
+//    }
+//    
+//    var method: Moya.Method {
+//        switch self {
+//        case .writeReview:
+//            return .post
+//        case .fetchMyReviewDetail:
+//            return .get
+//        }
+//    }
+//    
+//    var task: Moya.Task {
+//        switch self {
+//        case .writeReview(_, content: let content):
+//            return .requestJSONEncodable(content)
+//        case .fetchMyReviewDetail:
+//            return .requestPlain
+//        }
+//    }
+//    
+//    var headers: [String: String]? {
+//        switch self {
+//        default:
+//            return NetworkConstant.hasTokenHeader
+//        }
+//    }
+//    
+//    var validationType: ValidationType {
+//        return .successCodes
+//    }
+//}
