31469a64c9112de39a70c2d473f4f253885c5ff2
seongmin221 <seongmin221@naver.com>
2024-01-16 00:10:27 +0900

    [Refact] #227 - search service refact

--- GEON-PPANG-iOS/Global/Network/NewService/SearchService.swift
+++ GEON-PPANG-iOS/Global/Network/NewService/SearchService.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+import Moya
+
+enum SearchService {
+    case bakeries(bakeryName: String)
+}
+
+extension SearchService: GBService {
+    var domain: GBDomain {
+        return .search
+    }
+    
+    var urlPath: String {
+        switch self {
+        case .bakeries:
+            return "/bakeries"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .bakeries:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .bakeries(let bakeryName):
+            return .requestParameters(parameters: ["searchTerm": bakeryName],
+                                      encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headerType: GBHeaderFields {
+        switch self {
+        case .bakeries:
+            return .accessToken
+        }
+    }
+    
+}
--- GEON-PPANG-iOS/Global/Network/Service/Search/SearchService.swift
+++ GEON-PPANG-iOS/Global/Network/Service/Search/SearchService.swift
@@ -9,44 +9,44 @@ import Foundation
 
 import Moya
 
-enum SearchService {
-    case searchBakery(bakeryName: String)
-}
-
-extension SearchService: TargetType {
-    var baseURL: URL {
-        return URL(string: Config.baseURL)!
-    }
-    
-    var path: String {
-        switch self {
-        case .searchBakery:
-            return URLConstant.search
-        }
-    }
-    
-    var method: Moya.Method {
-        switch self {
-        case .searchBakery:
-            return .get
-        }
-    }
-    
-    var task: Moya.Task {
-        switch self {
-        case .searchBakery(bakeryName: let bakeryName):
-            return .requestParameters(parameters: ["searchTerm": bakeryName], encoding: URLEncoding.queryString)
-        }
-    }
-    
-    var headers: [String: String]? {
-        switch self {
-        default:
-            return NetworkConstant.header(.accessToken)
-        }
-    }
-    
-    var validationType: ValidationType {
-        return .successCodes
-    }
-}
+//enum SearchService {
+//    case searchBakery(bakeryName: String)
+//}
+//
+//extension SearchService: TargetType {
+//    var baseURL: URL {
+//        return URL(string: Config.baseURL)!
+//    }
+//    
+//    var path: String {
+//        switch self {
+//        case .searchBakery:
+//            return URLConstant.search
+//        }
+//    }
+//    
+//    var method: Moya.Method {
+//        switch self {
+//        case .searchBakery:
+//            return .get
+//        }
+//    }
+//    
+//    var task: Moya.Task {
+//        switch self {
+//        case .searchBakery(bakeryName: let bakeryName):
+//            return .requestParameters(parameters: ["searchTerm": bakeryName], encoding: URLEncoding.queryString)
+//        }
+//    }
+//    
+//    var headers: [String: String]? {
+//        switch self {
+//        default:
+//            return NetworkConstant.header(.accessToken)
+//        }
+//    }
+//    
+//    var validationType: ValidationType {
+//        return .successCodes
+//    }
+//}
