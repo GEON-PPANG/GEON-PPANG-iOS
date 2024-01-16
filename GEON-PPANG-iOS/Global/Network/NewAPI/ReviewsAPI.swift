9cc4821e5296cb866c785e067995c539f5e8434d
seongmin221 <seongmin221@naver.com>
2024-01-17 00:32:50 +0900

    [Refact] #227 - reviews api refact

--- GEON-PPANG-iOS/Global/Network/NewAPI/ReviewsAPI.swift
+++ GEON-PPANG-iOS/Global/Network/NewAPI/ReviewsAPI.swift
@@ -6,3 +6,67 @@
 //
 
 import Foundation
+
+import Moya
+
+typealias WriteReviewRequest = WriteReviewRequestDTO
+typealias WriteReviewResponse = GeneralResponse<VoidType>
+typealias MyReviewDetailResponse = GeneralResponse<MyReviewDetailResponseDTO>
+
+protocol ReviewsAPIType {
+    func postWriteReview(content: WriteReviewRequest, completion: @escaping (WriteReviewResponse?) -> Void)
+    func getMyReview(id: Int, completion: @escaping (MyReviewDetailResponse?) -> Void)
+}
+
+final class ReviewsAPI: ReviewsAPIType {
+    
+    static let shared: ReviewsAPI = ReviewsAPI()
+    private init() {}
+    
+    var provider: MoyaProvider<ReviewsService> = MoyaProvider(session: Session(interceptor: AuthInterceptor.shared),
+                                                              plugins: [MoyaLoggingPlugin()])
+    
+    func postWriteReview(content: WriteReviewRequest, completion: @escaping (WriteReviewResponse?) -> Void) {
+        provider.request(.write(bakeryID: content.bakeryID, content: content)) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    let response = try response.map(WriteReviewResponse.self)
+                    completion(response)
+                    
+                    #if DEBUG
+                    print("✅\(response)")
+                    #endif
+                }
+                catch let err {
+                    print(err.localizedDescription)
+                    completion(nil)
+                }
+            case let .failure(err):
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+    
+    func getMyReview(id: Int, completion: @escaping (MyReviewDetailResponse?) -> Void) {
+        provider.request(.myReview(reviewID: id)) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    let response = try response.map(MyReviewDetailResponse.self)
+                    completion(response)
+                    
+                    #if DEBUG
+                    print("✅\(response)")
+                    #endif
+                } catch let err {
+                    print(err.localizedDescription)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+}
