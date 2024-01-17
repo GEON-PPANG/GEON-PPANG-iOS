d94ef13222f1daf0d24fd6ab78088c9ab65b02e6
seongmin221 <seongmin221@naver.com>
2024-01-17 22:08:46 +0900

    [Refact] #227 - bookmarks api refactored

--- GEON-PPANG-iOS/Global/Network/NewAPI/BookmarksAPI.swift
+++ GEON-PPANG-iOS/Global/Network/NewAPI/BookmarksAPI.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+import Moya
+
+protocol BookmarksAPIType {
+    func bookmark(id: Int, request: BookmarkRequestDTO, completion: @escaping (Endpoint<BookmarkResponseDTO>?) -> Void)
+}
+
+final class BookmarksAPI: BookmarksAPIType {
+    
+    static let shared: BookmarksAPI = BookmarksAPI()
+    private init() {}
+    
+    var provider: MoyaProvider<BookmarksService> = MoyaProvider(session: Session(interceptor: AuthInterceptor.shared),
+                                                                plugins: [MoyaLoggingPlugin()])
+    
+    func bookmark(id: Int, request: BookmarkRequestDTO, completion: @escaping (Endpoint<BookmarkResponseDTO>?) -> Void) {
+        provider.request(.bookmark(bakeryID: id, request: request)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let response = try response.map(Endpoint<BookmarkResponseDTO>.self)
+                    completion(response)
+                } catch let err {
+                    print(err.localizedDescription)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+}
