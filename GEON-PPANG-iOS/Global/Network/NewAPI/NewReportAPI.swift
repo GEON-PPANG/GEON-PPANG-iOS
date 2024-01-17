874323b9840d0878836c15a878b0d154058837e5
jeongdung-eo <jeuniis2@gmail.com>
2024-01-17 22:04:12 +0900

    [Refact] #227 - Report API refactored

--- GEON-PPANG-iOS/Global/Network/NewAPI/NewReportAPI.swift
+++ GEON-PPANG-iOS/Global/Network/NewAPI/NewReportAPI.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+import Moya
+
+protocol ReportAPIType {
+    func postWriteReport(reviewID: Int, request: ReportRequestDTO, completion: @escaping (EndPoint<ReportResponseDTO>?) -> Void)
+}
+
+final class ReportAPI: ReportAPIType {
+    
+    static let shared: ReportAPI = ReportAPI()
+    private init() {}
+    
+    var provider = MoyaProvider<ReportService>(session: Session(interceptor: AuthInterceptor.shared), plugins: [MoyaLoggingPlugin()])
+    
+    func postWriteReport(reviewID: Int, request: ReportRequestDTO, completion: @escaping (EndPoint<ReportResponseDTO>?) -> Void) {
+        
+        provider.request(.writeReport(reviewID: reviewID, request: request)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let response = try response.map(EndPoint<ReportResponseDTO>.self)
+                    completion(response)
+                } catch let err {
+                    print(err.localizedDescription, 500)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+}
