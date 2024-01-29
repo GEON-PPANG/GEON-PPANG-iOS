bfe088e16046faa280d865db3fc5101724a47c4e
seongmin221 <seongmin221@naver.com>
2024-01-29 23:39:39 +0900

    [MERGE] #231 - conflict resolved

---
+++ GEON-PPANG-iOS/Presentation/Common/BottomSheet/LoginRequiredBottomSheet.swift
@@ -0,0 +1,38 @@
+//
+//  LoginRequiredBottomSheet.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 1/29/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class LoginRequiredBottomSheet: UIView {
+    
+    // MARK: - Property
+    
+    
+    
+    // MARK: - UI Property
+    
+    
+    
+    // MARK: - Life Cycle
+    
+    
+    
+    // MARK: - Setting
+    
+    
+    
+    // MARK: - Action Helper
+    
+    
+    
+    // MARK: - Custom Method
+    
+    
+    
+}
