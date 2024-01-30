404bd797a090daa86d8ea063d7e89a0139d01811
seongmin221 <seongmin221@naver.com>
2024-01-30 23:07:08 +0900

    [ADD] #231 - added some files

---
+++ GEON-PPANG-iOS/Global/Protocol/LoginProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  LoginProtocol.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 1/30/24.
+//
+
+import Foundation
---
+++ GEON-PPANG-iOS/Presentation/Common/BottomSheet/LoginRequiredViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginRequiredViewController.swift
+//  GEON-PPANG-iOS
+//
+//  Created by 이성민 on 1/30/24.
+//
+
+import Foundation
