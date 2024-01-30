537df78211874f40d7f81ff23571469e224599f6
seongmin221 <seongmin221@naver.com>
2024-01-30 23:18:40 +0900

    [CHORE] #231 - added login protocol

--- GEON-PPANG-iOS/Global/Protocol/LoginProtocol.swift
+++ GEON-PPANG-iOS/Global/Protocol/LoginProtocol.swift
@@ -5,4 +5,43 @@
 //  Created by 이성민 on 1/30/24.
 //
 
-import Foundation
+import UIKit
+
+protocol LoginProtocol {
+    func postSignUp(with request: SignUpRequestDTO, viewController: UIViewController, completion: (() -> Void)?)
+    func getNickname(_ completion: @escaping (String?) -> Void)
+}
+
+extension LoginProtocol {
+    
+    func postSignUp(with request: SignUpRequestDTO, viewController: UIViewController, completion: (() -> Void)?) {
+        
+        AuthAPI.shared.postSignUp(request: request) { status in
+            
+            guard let code = status?.code else { return }
+            switch code {
+            case 200...299:
+                guard let userID = status?.data?.memberID else { return }
+                AnalyticManager.set(userId: userID)
+                completion?()
+            default:
+                Utils.showAlert(title: "에러", description: "실패", at: viewController)
+            }
+        }
+    }
+    
+    func getNickname(_ completion: @escaping (String?) -> Void) {
+        
+        MemberAPI.shared.getNickname { result in
+            guard let result = result,
+                  let response = result.data
+            else { return }
+            switch result.code {
+            case 200:
+                completion(response.nickname)
+            default:
+                completion(nil)
+            }
+        }
+    }
+}
