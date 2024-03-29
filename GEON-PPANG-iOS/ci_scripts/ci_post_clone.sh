f799c22c6f8a1156e8e933228285888e83f323e7
jeongdung-eo <jeuniis2@gmail.com>
2024-03-29 13:18:33 +0900

    [Fix] script 경로 변경

--- GEON-PPANG-iOS/ci_scripts/ci_post_clone.sh
+++ GEON-PPANG-iOS/ci_scripts/ci_post_clone.sh
@@ -6,7 +6,7 @@
 #  Created by JEONGEUN KIM on 3/29/24.
 #  
 
-FOLDER_PATH="/Users/workspace/GEON-PPANG-iOS"
+FOLDER_PATH="/Volumes/workspace/repository/GEON-PPANG-iOS"
 
 # *.xconfig 파일 이름
 CONFIG_FILENAME="Config.xcconfig"
