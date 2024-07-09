89a8fc8537ce31815d7813f24e54a736b1c995ab
jeongdung-eo <jeuniis2@gmail.com>
2024-07-09 22:05:34 +0900

    [Feat] #275 - HomeViewModel 구현

---
+++ GEON-PPANG-iOS/Presentation/Scene/Home/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,85 @@
+//
+//  HomeViewModel.swift
+//  GEON-PPANG-iOS
+//
+//  Created by JEONGEUN KIM on 7/9/24.
+//
+
+
+import Foundation
+import Combine
+
+final class HomeViewModel: ViewModelType {
+    
+    struct Input {
+        let viewWillAppear: PassthroughSubject<Void, Never>
+        init(viewWillAppear: PassthroughSubject<Void, Never>) {
+            self.viewWillAppear = viewWillAppear
+        }
+    }
+    
+    struct Output {
+        let best: AnyPublisher<([BestBakery], [BestReview]), Error>
+    }
+    
+    // MARK: - Property
+    
+    private let usecase: HomeUseCase
+    private var cancellable: Set<AnyCancellable> = Set()
+    
+    init(usecase: HomeUseCase) {
+        self.usecase = usecase
+    }
+    
+    // MARK: - func
+    
+    func transform(_ input: Input) -> Output {
+        let bakery = input.viewWillAppear
+            .compactMap { [weak self] in self }
+            .flatMap { _ -> AnyPublisher<[BestBakery], Error> in
+                Future<[BestBakery], Error> { promise in
+                    Task {
+                        do {
+                            let bakery = try await self.fetchBestBakery()
+                            promise(.success(bakery))
+                        } catch {
+                            promise(.failure(error))
+                        }
+                    }
+                }
+                .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+        
+        let review = input.viewWillAppear
+            .compactMap { [weak self] in self }
+            .flatMap {  _ -> AnyPublisher<[BestReview], Error> in
+                return Future<[BestReview], Error> { promise in
+                    Task {
+                        do {
+                            let review = try await self.fetchBestReview()
+                            promise(.success(review))
+                        } catch {
+                            promise(.failure(error))
+                        }
+                    }
+                }
+                .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+        
+        let best = Publishers.CombineLatest(bakery, review)
+        
+        return Output(best: best.eraseToAnyPublisher())
+    }
+}
+
+extension HomeViewModel {
+    private func fetchBestBakery() async throws -> [BestBakery] {
+        return try await self.usecase.fetchBestBakeries()
+    }
+    
+    private func fetchBestReview() async throws -> [BestReview] {
+        return try await self.usecase.fetchBestReviews()
+    }
+}
